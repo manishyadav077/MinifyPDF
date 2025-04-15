3e870291b01aef115e033de98310fe04e8e8247f
manishyadav077 <136573039+manishyadav077@users.noreply.github.com>
2025-04-15 10:41:12 +0545

    navbar completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 import type { Metadata } from "next";
 import { Source_Sans_3 as FontSans } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/common/header";
+import Footer from "@/components/common/footer";
 
 const fontSans = FontSans({
   variable: "--font-sans",
   subsets: ["latin"],
-  weight: ['200', '300', '400', '500', '600', '700', '800', '900']
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
@@ -20,10 +22,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${fontSans.variable} font-sans antialiased`}
-      >
-        {children}
+      <body className={`${fontSans.variable} font-sans antialiased`}>
+        <div className="relative flex min-h-screen flex-col">
+          <Header />
+          <main className="flex-1">{children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
---
+++ components/common/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-gray-50 py-12'>
+      Hey, I am footer
+    </div>
+  )
+}
+
+export default Footer
