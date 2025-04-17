d6b04212894a536ae4d4ecd5d39f18f94653865f
manishyadav077 <136573039+manishyadav077@users.noreply.github.com>
2025-04-17 10:30:08 +0545

    authentication completed

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,6 +1,7 @@
 import { FileText } from "lucide-react";
 import { Button } from "../ui/button";
 import NavLink from "./nav-link";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 const Header = () => {
   const isLoggedIn = false;
@@ -17,22 +18,27 @@ const Header = () => {
 
       <div className="flex lg:justify-center gap-4 lg:gap-12 lg:items-center">
         <NavLink href="/#pricing">Pricing</NavLink>
-        {isLoggedIn && <NavLink href="/dashboard">Your Summaries</NavLink>}
+        <SignedIn>
+          {" "}
+          <NavLink href="/dashboard">Your Summaries</NavLink>
+        </SignedIn>
       </div>
 
       <div className="flex lg:justify-end lg:flex-1">
-        {isLoggedIn ? (
+        <SignedIn>
           <div className="flex gap-2 items-center">
             {" "}
             <NavLink href="/upload">Upload a PDF</NavLink>
             <div>Pro</div>
-            <Button>Sign Out</Button>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
           </div>
-        ) : (
-          <div>
-            <NavLink href="/sign-in">Sign In</NavLink>
-          </div>
-        )}
+        </SignedIn>
+
+        <SignedOut>
+          <NavLink href="/sign-in">Sign In</NavLink>
+        </SignedOut>
       </div>
     </nav>
   );
