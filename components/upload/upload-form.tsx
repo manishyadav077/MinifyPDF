a75b0ad25cacf786cc9a57d9f3911d07dda39c19
manishyadav077 <136573039+manishyadav077@users.noreply.github.com>
2025-04-20 12:58:18 +0545

    upload service architecture

--- components/upload/upload-form.tsx
+++ components/upload/upload-form.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+"use client";
+import UploadFormInput from "./upload-form-input";
+import { z } from "zod";
+
+const schema = z.object({
+  file: z
+    .instanceof(File, { message: "Invalid file" })
+    .refine((file) => file.size <= 20 * 1024 * 1024, {
+      message: "File size must be less than 20MB",
+    })
+    .refine(
+      (file) => file.type.startsWith("application/pdf"),
+      "File must be a PDF"
+    ),
+});
 
 const UploadForm = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get("file") as File;
+    const validatedFields = schema.safeParse({file})
 
-export default UploadForm
+    if(!validatedFields.success){
+      console.log(
+        validatedFields.error.flatten().fieldErrors.file?.[0] ?? 'Invalid File'
+      )
+      return
+    }
+    
+  };
+  return (
+    <div className="flex flex-col gap-8 w-full max-w-2xl mx-auto">
+      <UploadFormInput onSubmit={handleSubmit} />
+    </div>
+  );
+};
+
+export default UploadForm;
