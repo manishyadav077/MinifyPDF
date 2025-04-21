1f9148629f0a224d23f21fe25fd9bac773723954
manishyadav077 <136573039+manishyadav077@users.noreply.github.com>
2025-04-21 12:04:06 +0545

    file upload api route completed

--- components/upload/upload-form.tsx
+++ components/upload/upload-form.tsx
@@ -1,6 +1,8 @@
 "use client";
+import { useUploadThing } from "@/utils/uploadthing";
 import UploadFormInput from "./upload-form-input";
 import { z } from "zod";
+import { toast } from "sonner";
 
 const schema = z.object({
   file: z
@@ -14,21 +16,54 @@ const schema = z.object({
     ),
 });
 
-const UploadForm = () => {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+const UploadForm = async () => {
+  const { startUpload, routeConfig } = useUploadThing("pdfUploader", {
+    onClientUploadComplete: () => {
+      console.log("upload successfully!");
+    },
+    onUploadError: (err) => {
+      console.error("error occured while uploading", err);
+      toast("Error occured while uploading", {
+        description: err.message,
+      });
+    },
+    onUploadBegin: ({ file }) => {
+      console.log("upload has begun for", file);
+    },
+  });
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const file = formData.get("file") as File;
-    const validatedFields = schema.safeParse({file})
+    const validatedFields = schema.safeParse({ file });
 
-    if(!validatedFields.success){
-      console.log(
-        validatedFields.error.flatten().fieldErrors.file?.[0] ?? 'Invalid File'
-      )
-      return
+    if (!validatedFields.success) {
+      toast.error("Something went wrong", {
+        description:
+          validatedFields.error.flatten().fieldErrors.file?.[0] ??
+          "Invalid File",
+      });
+      return;
     }
-    
+
+    toast("Uploading PDF", {
+      description: "We are uploading your PDF!",
+    });
+
+    const resp = await startUpload([file]);
+    if (!resp) {
+      toast.error("Something went wrong", {
+        description: "Please use different file",
+      });
+      return;
+    }
+
+    toast("processing PDF", {
+      description: "Hang tight! Our AI is reading through your document!",
+    });
   };
+
   return (
     <div className="flex flex-col gap-8 w-full max-w-2xl mx-auto">
       <UploadFormInput onSubmit={handleSubmit} />
